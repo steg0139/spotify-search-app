f93e3ae2535cdeaac6becaa7aea793b0b3d93525
Josh Stegman <jsteg21@gmail.com>
2021-07-09 10:20:19 -0500

    Search functionality working

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import Header from './Header';
+
 const Home = (props) => {
   const {
     REACT_APP_CLIENT_ID,
